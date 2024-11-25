60183ecd8e081d2b31dde342efdd19c33b2c8288
Jacky Lee <leeja12@oocl.com>
2024-11-25 20:04:00 +0800

    refactor: inport removal and make repo final

--- src/main/java/com/oocl/springbootemployee/controller/CompanyController.java
+++ src/main/java/com/oocl/springbootemployee/controller/CompanyController.java
@@ -1,19 +1,17 @@
 package com.oocl.springbootemployee.controller;
 
 import com.oocl.springbootemployee.model.Company;
-import com.oocl.springbootemployee.model.Employee;
-import com.oocl.springbootemployee.model.Gender;
 import com.oocl.springbootemployee.repository.CompanyRepository;
-import com.oocl.springbootemployee.repository.EmployeeRepository;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/companies")
 public class CompanyController {
-    private CompanyRepository companyRepository;
+    private final CompanyRepository companyRepository;
 
     public CompanyController(CompanyRepository companyRepository) {
         this.companyRepository = companyRepository;
